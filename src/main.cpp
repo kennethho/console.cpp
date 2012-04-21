e03768ed0648fa9c96572de0962e5aafcd745d47
Wei Cheng Pan <legnaleurc@gmail.com>
2012-04-21 17:01:54 +0800

    Change DEMO feature.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 int main() {
 	Console console;
 	int i;
-	console >> i << " i is " << i << '\n';
+	console << "input a integer: " >> i << "i is " << i << '\n';
+	double d;
+	console << "input a floating number: " >> d << "d is " << d << '\n';
+	std::string s;
+	console << "input a string: " >> s << "s is " << s << '\n';
 	return 0;
 }
