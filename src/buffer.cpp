48218245eaf86a3b9bda72ac23ff954c06a28fcf
Wei Cheng Pan <legnaleurc@gmail.com>
2012-04-21 15:33:20 +0800

    Add buffer implementation.

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -1,11 +1,36 @@
-#include "buffer_p.hpp"
+#include "buffer.hpp"
 
-Buffer::Private::Private( std::streambuf * in, std::streambuf * out ):
-in( in ),
-out( out ) {
+Buffer::Buffer( std::streambuf * in, std::streambuf * out ): std::streambuf(), in_( in ), out_( out ) {
 }
 
-Buffer::Buffer( std::streambuf * in, std::streambuf * out ): std::streambuf(), p_( new Private( in, out ) ) {
+std::streamsize Buffer::showmanyc() {
+	return this->in_->in_avail();
+}
+
+std::streamsize Buffer::xsgetn( char * s, std::streamsize n ) {
+	return this->in_->sgetn( s, n );
+}
+
+int Buffer::underflow() {
+	return this->in_->sgetc();
+}
+
+int Buffer::uflow() {
+	int c = this->in_->sgetc();
+	this->in_->snextc();
+	return c;
+}
+
+int Buffer::pbackfail( int c ) {
+	return this->std::streambuf::pbackfail( c );
+}
+
+std::streamsize Buffer::xsputn( const char * s, std::streamsize n ) {
+	return this->out_->sputn( s, n );
+}
+
+int Buffer::overflow( int c ) {
+	return this->std::streambuf::overflow( c );
 }
 
 Buffer::~Buffer() {
--- src/buffer.hpp
+++ src/buffer.hpp
@@ -2,18 +2,27 @@
 #define BUFFER_HPP
 
 #include <streambuf>
-#include <memory>
 
 class Buffer : public std::streambuf {
 public:
 	Buffer( std::streambuf * in, std::streambuf * out );
 	virtual ~Buffer();
 
+protected:
+	virtual std::streamsize showmanyc();
+	virtual std::streamsize xsgetn( char * s, std::streamsize n );
+	virtual int underflow();
+	virtual int uflow();
+	virtual int pbackfail( int c = EOF );
+
+	virtual std::streamsize xsputn( const char * s, std::streamsize n );
+	virtual int overflow( int c = EOF );
+
 private:
 	Buffer( const Buffer & );
 	Buffer & operator =( const Buffer & );
-	class Private;
-	std::unique_ptr< Private > p_;
+	std::streambuf * in_;
+	std::streambuf * out_;
 };
 
 #endif
